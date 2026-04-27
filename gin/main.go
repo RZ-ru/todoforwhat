b6fddf0873e1c383a868552045fcd6d39f6a3227
Roman <roman.zakruzhnoy@gmail.com>
2026-04-27 18:17:41 +0700

    add rabbitMQ queue

--- gin/main.go
+++ gin/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
+	"todo/gin/internal/consumer"
 	"todo/gin/internal/handlers"
 	"todo/gin/internal/repository"
 	"todo/gin/internal/services"
 
 	_ "github.com/lib/pq"
+	amqp "github.com/rabbitmq/amqp091-go"
 
 	"github.com/gin-gonic/gin"
 )
@@ -36,9 +39,66 @@ func main() {
 	}
 	log.Println("DB connected")
 
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"tasks_exchange",
+		"fanout",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = ch.QueueDeclare(
+		"tasks", // имя очереди
+		true,    // durable (сохраняется после перезапуска)
+		false,   // auto delete
+		false,   // exclusive
+		false,   // no wait
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ch.QueueBind(
+		"tasks",          // очередь
+		"",               // routing key (не нужен для fanout)
+		"tasks_exchange", // exchange
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	repo := repository.NewPostgresTaskRepository(db)
 	svc := services.NewTaskService(repo)
 	handler := handlers.NewTaskHandler(svc)
+
+	ctx := context.Background()
+
+	cons := consumer.NewConsumer(ch)
+
+	go func() {
+		if err := cons.Start(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	server := gin.Default()
 
