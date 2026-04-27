f1df67d47353a04bdae1b5d819ec4efe6258ceb0
Roman <roman.zakruzhnoy@gmail.com>
2026-04-27 18:21:04 +0700

    add consumer for rabbitMQ

---
+++ gin/internal/consumer/consumer.go
@@ -0,0 +1,42 @@
+package consumer
+
+import (
+	"context"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	ch *amqp.Channel
+}
+
+func NewConsumer(ch *amqp.Channel) *Consumer {
+	return &Consumer{ch: ch}
+}
+
+func (c *Consumer) Start(ctx context.Context) error {
+
+	msgs, err := c.ch.Consume(
+		"tasks", // очередь
+		"",      // consumer name
+		true,    // auto-ack (пока так)
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+
+		case msg := <-msgs:
+			log.Printf("received: %s\n", string(msg.Body))
+		}
+	}
+}
